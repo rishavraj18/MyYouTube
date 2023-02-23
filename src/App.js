898fa29151876ac8d816c21d6586e2a8428833ad
rishavraj <fullstackdev.rishav@gmail.com>
2023-02-23 17:27:59 +0530

    Application page blueprint

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ function App() {
   return (
     <div>  
       <h1 className="bg-red-900">Test</h1>
+      {
+        /**
+         * Header
+         * Body
+         *  -> SideBar
+         *     + MenuItems
+         *  -> Main Container
+         *     + ButtonsLists
+         *     + VideoConatiner
+         *         - VideoCard
+         */
+      }
     </div>
   );
 }
