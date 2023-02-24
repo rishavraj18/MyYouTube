e8f867b3e5cbe093a8c1b9afcf053e320107e352
rishavraj <fullstackdev.rishav@gmail.com>
2023-02-24 14:05:22 +0530

    Added menu bar list items

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,36 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className="p-5 shadow-lg w-48">
+      <h1 className="">Home</h1>
+      <h1 className="pt-3">Shorts</h1>
+      <h1 className="pt-3">Subscriptions</h1>
+      <h1 className="pt-3">Library</h1>
+      <h1 className="pt-3">History</h1>
+      <h1 className="pt-3">Watch Later</h1>
+      <h1 className="pt-3">Liked Videos</h1>
+
+      <h1 className="font-bold pt-5">Subscriptions</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sport</li>
+        <li>Gaming</li>
+        <li>Films</li>
+      </ul>
+      <h1 className="font-bold pt-5">Explore</h1>
+      <ul>
+        <li>Trending</li>
+        <li>Shopping</li>
+        <li>Music</li>
+        <li>Films</li>
+        <li>Live</li>
+        <li>Gaming</li>
+        <li>News</li>
+        <li>Sport</li>
+        <li>Learning</li>
+        <li>Fashion & beauty</li>
+      </ul>
+    </div>
   )
 }
 
