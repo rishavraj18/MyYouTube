130b5534087184f6b0752a528bde39b27f3a4d5e
rishavraj <fullstackdev.rishav@gmail.com>
2023-02-24 10:58:53 +0530

    Added images and import in component

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import Menu from '../images/menu.png';
+import YTLogo from '../images/YTLogo.png'
+import User from '../images/user.jpg'
 
 const Head = () => {
   return (
-    <div>Head</div>
+    <div>
+      <div>
+        <img alt="menu" src={Menu}/>
+        <img alt="ytlogo" src={YTLogo}/>
+      </div>
+      <div>
+        <input type="text" />
+        <button>Search</button>
+      </div>
+      <div>
+        <img alt="user" src={User}/>
+      </div>
+    </div>
   )
 }
 
