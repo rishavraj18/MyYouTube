c6380af3951666b8d18c5bdcac43f72a4efadfb5
rishavraj <fullstackdev.rishav@gmail.com>
2023-03-01 01:31:38 +0530

    React router dom for watch page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Body from './components/Body';
 import Head from './components/Head';
+import MainContainer from './components/MainContainer';
+import WatchPage from './components/WatchPage';
 import store from './utils/store';
+
+const appRouter = createBrowserRouter([{
+  path: "/",
+  element: <Body/>,
+  children:[
+    {
+      path: "/",
+      element: <MainContainer />
+    },
+    {
+      path: "watch",
+      element: <WatchPage />
+    }
+  ]
+}]);
 
 function App() {
   return (
     <Provider store={store}>
     <div>  
       <Head/>
-      <Body/>
+      <RouterProvider router={appRouter}/>
       {
         /**
          * Header
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const VideoCard = ({ info }) => {
     console.log(info);
-    const {snippet, statistics} = info;
+    const {snippet, statistics, id} = info;
     const {channelTitle, title, thumbnails } = snippet;
 
     let calViews = parseInt(statistics.viewCount);
@@ -17,14 +17,19 @@ const VideoCard = ({ info }) => {
 
   return (
     <div className="p-2 m-1 w-52 shadow-lg">
-        <img className="rounded-xl" alt="card" src={thumbnails.medium.url} />
+        <img className="rounded-xl cursor-pointer" alt="card" src={thumbnails.medium.url}  onClick={() => playVideo(id)}/>
         <ul>
-            <li className="font-bold  text-sm">{title}</li>
+            <li className="font-bold cursor-pointer text-sm" onClick={() => playVideo(id)}>{title}</li>
             <li className="pt-2 text-sm">{channelTitle}</li>
             <li className="pt-2 text-sm">{countViews}</li>
         </ul>
     </div>
   )
+};
+
+function playVideo(id) {
+  var url = "https://www.youtube.com/watch?v="+id;
+  window.open(url);
 };
 
 export default VideoCard;
