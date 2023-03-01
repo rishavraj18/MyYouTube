46fee15d11ad548182831ee8c3dede5ab048e65e
rishavraj <fullstackdev.rishav@gmail.com>
2023-03-01 13:00:37 +0530

    Made changes for hiding sidebar menu while redirecting to video play page

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { closeMenu } from '../utils/appSlice';
 
 const WatchPage = () => {
+  const dispatch = useDispatch();
+  useEffect(()=> {
+    dispatch(closeMenu());
+  }, []);
   return (
     <div>WatchPage</div>
   )
 }
 
-export default WatchPage
+export default WatchPage;
