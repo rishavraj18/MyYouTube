9f17a988a07b332515df08c8e59390fc8f80485a
rishavraj <fullstackdev.rishav@gmail.com>
2023-03-02 15:36:09 +0530

    API call to search YT videos through debouncing

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Menu from '../images/menu.png';
 import YTLogo from '../images/YTLogo.png'
 import User from '../images/user.jpg'
 import Search from '../images/search.png';
 import { useDispatch } from 'react-redux';
 import { toggleMenu } from '../utils/appSlice';
+import { YOUTUBE_SEARCH_API } from '../utils/constants';
 
 const Head = () => {
+  const [searchQuery, setsearchQuery] = useState("");
+
+  useEffect(() => {   
+    const getSearchSuggestions = async () => {
+      console.log(searchQuery);
+      const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+      const json = await data.json();
+      //console.log(json[1]);
+    };
+    /**
+     *  Make an API call after every keypress but if the difference between 2 API calls is <200ms 
+     *  else decline the API call
+     * 
+     * key - i
+     * - render the component
+     * - useEffect();
+     * - start timer => make api call after 200ms
+     * 
+     * key - ip
+     * - destroy the component(useEffect return method)
+     * - re-render the component
+     * - useEffect();
+     * - start timer => make api call after 200ms
+     * 
+     */
+     // API call
+    const timer = setTimeout(() => getSearchSuggestions(), 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+  
+  
+
   const dispatch = useDispatch();
 
   const toggleMenuHandler = () => {
@@ -24,7 +60,12 @@ const Head = () => {
         </a>
       </div>
       <div className="col-span-10 px-10">
-        <input className="w-1/2 border border-gray-400 pt-1.5 pb-1.5 rounded-l-full" type="text" />
+        <input 
+        className = "w-1/2 border border-gray-400 pt-1.5 pb-1.5 pl-2 rounded-l-full" 
+        type = "text" 
+        value = {searchQuery}
+        onChange = {(e) => setsearchQuery(e.target.value)}
+        />
         <button className="border border-gray-400 pt-1 pb-3 pl-6 rounded-r-full bg-gray-200">
           <img className="h-5 pr-5 pt-1.5" alt="search" src={Search}/>
         </button>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,9 @@
 const myYTAPIKey = process.env.REACT_APP_YOUTUBE_API_KEY;
 
-
-
 export const YOUTUBE_VIDEOS_API = 
 "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+
 myYTAPIKey;
 
+export const YOUTUBE_SEARCH_API =
+"http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
