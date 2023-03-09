ec44d954c6a7ae2d5ffcba352c35151b14fd2702
rishavraj <fullstackdev.rishav@gmail.com>
2023-03-09 12:08:25 +0530

    Build code - v2

--- src/components/Head.js
+++ src/components/Head.js
@@ -14,10 +14,8 @@ const Head = () => {
 
   useEffect(() => {   
     const getSearchSuggestions = async () => {
-      console.log(searchQuery);
       const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
       const json = await data.json();
-      //console.log(json[1]);
       setSuggestions(json[1]);
     };
     /**
