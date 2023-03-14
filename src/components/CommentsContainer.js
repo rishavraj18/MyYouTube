cb4d5bba924121ae56a34e14a5a82ceab04e0ce1
rishavraj <fullstackdev.rishav@gmail.com>
2023-03-14 15:39:20 +0530

    Black border to gray border

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -123,7 +123,7 @@ const CommentsList = ({comments}) => {
   return comments.map((comment, index) => (
   <div>
     <Comment key={index} data={comment} />
-     <div className="pl-5 border border-l-black ml-5">
+     <div className="pl-5 border border-l-gray-300 ml-5">
       <CommentsList comments={comment.replies} />
      </div>
   </div>
